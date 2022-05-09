115cbf3f38286fd972f031b2f33d37f4f2373a4d
NiNoX14 <hardstuckd5lul@gmail.com>
2022-05-09 18:48:16 +0300

    feat: fix cors and db config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,15 +7,21 @@ dotenv.config({
 });
 
 export default {
+  // type: 'postgres',
+  // host: 'db',
+  // port: parseInt(process.env.POSTGRES_PORT as string, 10) as number,
+  // username: process.env.POSTGRES_USER as string,
+  // password: process.env.POSTGRES_PASSWORD as string,
+  // database: process.env.POSTGRES_DB as string,
   type: 'postgres',
-  host: 'db',
-  port: parseInt(process.env.POSTGRES_PORT as string, 10) as number,
   cache: false,
-  username: process.env.POSTGRES_USER as string,
-  password: process.env.POSTGRES_PASSWORD as string,
-  database: process.env.POSTGRES_DB as string,
+  url: process.env.DATABASE_URL as string,
+  // url: 'postgres://npqesdfgcyfqts:e6a39ac1627d764cac34de7d8e76efc681ffcdbc2a21c121f7e0d6bab50fbcfb@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d8dr3l1rfiupob',
   synchronize: false,
   logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: ['src/resources/**/**.entity{.ts,.js}'],
   migrations: ['./migrations/*.ts'],
 } as ConnectionOptions;
